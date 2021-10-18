daa7187906a60895bd63ee5c6888c5a0aa158c67
Swati-Verma03 <Vswati.0399@gmail.com>
2021-10-18 14:27:53 +0530

    adding base entity. closes #10

--- pet-clinic-web/src/test/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,6 +2,8 @@ package swati.springframework1.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.Runwith;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
